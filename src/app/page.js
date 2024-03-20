10f91f11b1f74abcb5c1984bd3b24a8b597e0a3f
Sakkar Chowdhury <Sakkhor.Chowdhury@gmail.com>
2024-03-20 12:56:00 +0600

    Navigation with Link Component

--- src/app/page.js
+++ src/app/page.js
@@ -1,4 +1,5 @@
 import Counter from "@/components/Counter/Counter"
+import Link from "next/link"
 
 function HomePage() {
   // throw new Error()
@@ -6,6 +7,12 @@ function HomePage() {
     <div>
       <h1>This is Next App</h1>
       <Counter/>
+      <Link href="/about">
+      <button className="btn btn-accent">About</button>
+      </Link>
+      <Link href="/contact">
+      <button className="btn btn-info">Contact</button>
+      </Link>
     </div>
   )
 }
--- src/components/Counter/Counter.js
+++ src/components/Counter/Counter.js
@@ -5,10 +5,10 @@ function Counter() {
   const [counter,setCounter] = useState(0)
   return (
     <div>
-      <h2 className='m-4'>Counter: {counter}</h2>
+      <h2>Counter: {counter}</h2>
 
-      <button className='btn btn-primary m-4' onClick={()=>setCounter(counter + 1)}>Increase</button>
-      <button className='btn btn-primary m-4' onClick={()=>setCounter(counter - 1)}>Decrese</button>
+      <button className='btn btn-primary' onClick={()=>setCounter(counter + 1)}>Increase</button>
+      <button className='btn btn-primary' onClick={()=>setCounter(counter - 1)}>Decrese</button>
       
     </div>
   )
